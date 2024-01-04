de639ffb3442929ee32ca3714c7be5180a15c5e3
Bhushan1010 <bhushansisode11@gmail.com>
2024-01-04 23:08:43 +0530

    Leet Code Commit

---
+++ 4th Jan 24.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        mp =Counter(nums)
+        res =0 
+        for v in mp.values():
+            if v==1:
+                return -1 
+            res +=math.ceil(v/3)
+        return res
